1a7ac1c11404eecefd106c46bfc3515071c9a73b
paul <paul.andoman@gmail.com>
2013-10-24 19:52:38 +1100

    Adding Files

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 QuickBlog::Application.routes.draw do
   devise_for :admin_users, ActiveAdmin::Devise.config
-  ActiveAdmin.routes(self)
-
+  
   root :to => 'posts#index'
+
+  ActiveAdmin.routes(self)
 
   resources :posts do
     resources :comments, :only => [:create]
